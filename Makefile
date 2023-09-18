3971add6b75571510e37093807f6bee13ef16fe3
sam0verfl0w <samtalespro@gmail.com>
2023-09-18 13:16:54 +0200

    update

--- Makefile
+++ Makefile
@@ -19,10 +19,24 @@ stop:
 down:
 	@docker compose down --rmi all
 
+rm:
+	@rm -rf backend/dist
+	@rm -rf frontend/dist
+	@rm -rf volumes
+
 build:
 	@docker compose -f docker-compose.yml build
 
 build_dev:
 	@docker compose -f docker-compose-dev.yml build
 
-.PHONY: all dev stop down build build_dev
+lint:
+	cd backend && @npm run lint
+
+format:
+	cd backend && @npm run format
+
+re:
+	sudo make stop down rm format lint up_dev
+
+.PHONY: all dev stop down build build_dev lint format re
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
