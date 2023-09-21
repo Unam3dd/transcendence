31dbf4bc8f81c9eab2927432ce4545fed55ccaaa
sam0verfl0w <samtalespro@gmail.com>
2023-09-21 10:19:24 +0200

    update frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: '<h1>Hello world</h1>'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Transcendence';
 }
