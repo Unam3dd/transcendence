9f8f001096af5d0437679b1081188f20b8b49c50
sam0verfl0w <samtalespro@gmail.com>
2023-09-22 13:05:10 +0200

    Updating auth interfaces

--- backend/src/interfaces/auth.interfaces.ts
+++ backend/src/interfaces/auth.interfaces.ts
@@ -1,9 +1,7 @@
-interface AuthTo42Data {
+export interface AuthTo42Data {
   grant_type: string;
   client_id: string;
   client_secret: string;
   code: string;
   redirect_uri: string;
-}
-
-export { AuthTo42Data };
+};
