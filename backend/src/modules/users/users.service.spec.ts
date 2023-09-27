2fe7e004b5dec0a55897190616d782bd6001e52c
sam0verfl0w <samtalespro@gmail.com>
2023-09-27 23:03:28 +0200

    First patch of users.service.spec.ts im gonna write all test for user services

--- backend/src/modules/users/users.service.spec.ts
+++ backend/src/modules/users/users.service.spec.ts
@@ -2,26 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { UsersModule } from './users.module';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let userService: UsersService;
-  let userRepository: User
+  let mockUserRepository: Repository<User>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService, {
         provide: getRepositoryToken(User),
-        useClass: UsersModule
+        useValue: mockUserRepository
       }],
     }).compile();
 
     userService = module.get<UsersService>(UsersService);
-    userRepository = module.get<User>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(userService).toBeDefined();
-    expect(userRepository).toBeDefined();
   });
 });
