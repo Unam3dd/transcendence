daccf09d30a9fe6a258d1dd00d77652158e80233
sam0verfl0w <samtalespro@gmail.com>
2023-09-28 11:23:52 +0200

    Do some test on controller user part

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -5,21 +5,13 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Response } from 'express';
-import { HttpStatus } from '@nestjs/common';
 
 describe('UsersController', () => {
   let usersController: UsersController;
   let usersService: UsersService;
   let mockUserRepository: Repository<User>;
-  let mockResponse: Partial<Response>;
 
   beforeEach(async () => {
-
-    mockUserRepository = {
-      create: jest.fn(),
-      save: jest.fn(),
-    } as any;
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -32,80 +24,33 @@ describe('UsersController', () => {
 
     usersController = module.get<UsersController>(UsersController);
     usersService = module.get<UsersService>(UsersService);
-
-    mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
-    };
   });
 
-  it('shoudl return 201 if User is successfully created', async () => {
+  it('Should return Array of Users in Database', async () => {
     const mockUserDto : CreateUserDto = {
-      id: 1,
       login: "chjoie",
       firstName: "charles",
       lastName: "joie",
       nickName: "mologue",
       email: "chjoie@42.fr",
-      a2f: true,
+      a2f: false,
       avatar: "http://google.com"
     };
 
-    const mockUser : User = {
-      id: 1,
-      login: "chjoie",
-      firstName: "charles",
-      lastName: "joie",
-      nickName: "mologue",
-      email: "chjoie@42.fr",
-      a2f: true,
-      avatar: "http://google.com",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    };
+    let arr_user: User[];
 
-    jest.spyOn(mockUserRepository, 'create').mockReturnValue(mockUser);
-    jest.spyOn(mockUserRepository, 'save').mockResolvedValue(mockUser);
+    const mockFunctions = {
+      findAll: jest.fn()
+    }
 
-    jest.spyOn(usersService, 'registerUser').mockResolvedValue(undefined);
-
-    await usersController.RegisterNewUser(mockUserDto, mockResponse as Response);
-
-    expect(mockResponse.status).toHaveBeenCalledWith(HttpStatus.CREATED);
-  });
-
-  it('Should return 409 if user cant be created', async () => {
-    const mockUserDto : CreateUserDto = {
-      id: 1,
-      login: "chjoie",
-      firstName: "charles",
-      lastName: "joie",
-      nickName: "mologue",
-      email: "chjoie@42.fr",
-      a2f: true,
-      avatar: "link"
-    };
-
-    const mockUser : User = {
-      id: 1,
-      login: "chjoie",
-      firstName: "charles",
-      lastName: "joie",
-      nickName: "mologue",
-      email: "chjoie@42.fr",
-      a2f: true,
-      avatar: "link",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    };
-
-    jest.spyOn(mockUserRepository, 'create').mockReturnValue(mockUser);
-    jest.spyOn(mockUserRepository, 'save').mockResolvedValue(mockUser);
-
-    jest.spyOn(usersService, 'registerUser').mockRejectedValue(new Error("Error thrown when trying to create user"));
-
-    await usersController.RegisterNewUser(mockUserDto, mockResponse as Response);
-
-    expect(mockResponse.status).toHaveBeenCalledWith(HttpStatus.CONFLICT);
-  });
+    for (let i = 0; i < 10; i++) {
+      
+      mockUserDto.id = i;
+      mockUserDto.login = `${mockUserDto.login}${(Math.random() + 1).toString(36).substring(1)}`;
+      mockUserDto.nickName = `${(Math.random() + 1).toString(36).substring(1)}`;
+      mockUserDto.a2f = (i & 0x1) ? true : false;
+      
+      console.log(`Register new User ${mockUserDto.login} ${mockUserDto.nickName}`);
+    }
+  })
 });
