157079c5804ff6f15c7759e9dac5383bb509cda5
chjoie <charlesjoie.b@gmail.com>
2023-10-02 12:01:46 +0200

    commented all methods for users controller and service

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -16,10 +16,12 @@ import { Response } from 'express';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { isEmpty } from 'class-validator';
 
+//This controller handle client requests from the /users route
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // Recieving a POST request to create a new user
   @Post()
   async RegisterNewUser(@Body() User: CreateUserDto, @Res() res: Response) {
     try {
@@ -30,6 +32,7 @@ export class UsersController {
     }
   }
 
+  // Recieving a PUT request to update informations about an user
   @Put()
   async UpdateUserInfo(@Body() User: UpdateUserDto, @Res() res: Response) {
     try {
@@ -40,17 +43,20 @@ export class UsersController {
     }
   }
 
+  // Recieving a GET request to get all users data
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  // Recieving a GET request to get informations about one user 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const user = await this.usersService.findOne(id);
     return isEmpty(user) ? {} : user;
   }
 
+  // Recieving a DELETE request to delete one user
   @Delete(':id')
   async deleteUser(
     @Param('id', ParseIntPipe) id: number,
