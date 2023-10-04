5d62e0cbcc634c5037428e9f0abbcd0f2dcdeed2
Fenbjorn <ngrenoux@gmail.com>
2023-10-04 13:44:13 +0200

    update

--- frontend/src/app/services/profile-page.service.ts
+++ frontend/src/app/services/profile-page.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import { CookiesService } from './cookies.service';
 
 @Injectable({
   providedIn: 'root'
