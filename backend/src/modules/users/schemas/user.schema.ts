862f9fc26b3b4225ae3249ccab6d3a2559011578
chjoie <charlesjoie.b@gmail.com>
2023-10-05 19:20:48 +0200

    started API documentation with swagger

---
+++ backend/src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsString,
+    IsBoolean,
+    IsNumber,
+    IsOptional,
+    IsEmail,
+    IsUrl,
+  } from 'class-validator';
+  
+  export class UserSchema {
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    id?: number;
+  
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    login?: string | null;
+  
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    firstName?: string | null;
+  
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    lastName?: string | null;
+  
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    nickName?: string | null;
+  
+    @ApiProperty()
+    @IsEmail()
+    @IsOptional()
+    email?: string | null;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    a2f: boolean;
+  
+    @ApiProperty()
+    @IsOptional()
+    @IsUrl()
+    avatar: string;
+  }
