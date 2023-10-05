6a0bc23241ba2e91474fe1d774fa11017a5de05c
chjoie <charlesjoie.b@gmail.com>
2023-10-05 19:20:48 +0200

    started API documentation with swagger

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -18,13 +18,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { isEmpty } from 'class-validator';
 import { User } from './entities/user.entity';
 import { AuthGuard } from '../auth/auth.guard';
+import { UserSchema } from './schemas/user.schema';
+import { ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNoContentResponse, ApiOkResponse, ApiOperation, ApiParam, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard)
+@ApiTags('Users Module')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({summary: 'Register a new user'})
+  @ApiCreatedResponse( {description: 'User Created'})
+  @ApiConflictResponse({ description: 'User Already Exists' })
+  @ApiUnauthorizedResponse({description: 'Unauthorized'}) 
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
+  @ApiBody({ type: UserSchema } )
   async RegisterNewUser(@Body() User: CreateUserDto, @Res() res: Response) {
     try {
       await this.usersService.registerUser(User);
@@ -35,6 +44,12 @@ export class UsersController {
   }
 
   @Put()
+  @ApiOperation({summary: 'Update user informations'})
+  @ApiCreatedResponse( {description: 'User succesfully updated'})
+  @ApiResponse({ status:309, description: 'User not modified' })
+  @ApiUnauthorizedResponse({description: 'Unauthorized'}) 
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
+  @ApiBody({ type: UserSchema } )
   async UpdateUserInfo(@Body() User: UpdateUserDto, @Res() res: Response) {
     try {
       await this.usersService.updateUser(User.id, User);
@@ -45,17 +60,32 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({summary: 'Get all users informations'})
+  @ApiOkResponse( {description: 'Return all users informations'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized'}) 
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
   async findAll(@Res() res: Response) {
     return res.status(HttpStatus.OK).send(await this.usersService.findAll());
   }
 
   @Get(':id')
+  @ApiOperation({summary: 'Return an user'})
+  @ApiParam({name: 'id', type: 'number', description: 'The user id' })
+  @ApiOkResponse({description: 'User informations'})
+  @ApiUnauthorizedResponse({description: 'Unauthorized'}) 
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
   async findOne(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
     const user: User = await this.usersService.findOne(id);
     return res.status(HttpStatus.OK).send(isEmpty(user) ? {} : user);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Delete an user'})
+  @ApiParam({ name: 'id', type: 'number', description: 'The user id' })
+  @ApiCreatedResponse( {description: 'User Deleted'})
+  @ApiNoContentResponse({ description: 'User not found' })
+  @ApiUnauthorizedResponse({description: 'Unauthorized'}) 
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
   async deleteUser(
     @Param('id', ParseIntPipe) id: number,
     @Res() res: Response,
