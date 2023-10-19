39b07c9325949fc5aaca4c1f891524c3b57173da
chjoie <charlesjoie.b@gmail.com>
2023-10-19 13:53:05 +0200

    friends component in development

--- frontend/src/app/friends/friends.component.ts
+++ frontend/src/app/friends/friends.component.ts
@@ -7,55 +7,83 @@ import { UserSanitizeInterface } from '../interfaces/user.interface';
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss']
 })
+
 export class FriendsComponent implements OnInit {
 
   constructor(private readonly requestsService: RequestsService) {}
 
-  approvedfriendsList: Friends[] = [];
-  pendingfriendsList: Friends[] = [];
+  friendsList: Friends[] = [];
   approvedFriends: UserSanitizeInterface[] = [];
   pendingFriends: UserSanitizeInterface[] = [];
 
   showContent: boolean = false;
+  
+  showOptions: boolean = false;
+  showPendingOptions: boolean = false;
+  toggleOptions: boolean = false;
+  togglePendingOptions: boolean = false;
 
   ngOnInit() {
-    //get all approved friends, then stocks their infos in approvedFriends array
-    this.requestsService.listFriends(true).subscribe((friends) => {
-      this.approvedfriendsList = friends;
-      this.createApprovedList();
-    });
+    this.fetchData();
+  }
 
-    //get all friends, then stocks only pending friends in pendingFriends array
+  fetchData() {
+
+    //get all friends, then stocks pending / approved friends in two diffrents arrays
     this.requestsService.listFriends(false).subscribe((friends) => {
-      this.pendingfriendsList = friends;
-      console.log(this.pendingfriendsList);
-      this.createPendingList();
+      this.friendsList = friends;
+      this.createList();
     });
   }
 
-  createApprovedList() {
-  // Loop on each approved friends, then push their users infos into approvedFriends
-    this.approvedfriendsList.forEach((element) => {
-      this.requestsService.getUserInfo(element.user2).subscribe((user) => {
-        this.approvedFriends.push(user);
-      });
-    })
-  }
-
-  createPendingList() {
-  // Loop on each friends, then push only pending into pendingFriends
-    this.pendingfriendsList.forEach((element) => {
-      if (element.status === false)
-      {
+  createList() {
+  // Loop on each friends, then push pending into pendingFriends array or approved into approvedFriends array
+    this.friendsList.forEach((element) => {
+      if (element.status === false) {
         this.requestsService.getUserInfo(element.user2).subscribe((user) => {
           this.pendingFriends.push(user);
         });
       }
-    })
+     else {
+        this.requestsService.getUserInfo(element.user2).subscribe((user) => {
+          this.approvedFriends.push(user);
+        });
+      }
+    });
   }
 
   toggleContent() {
     this.showContent = !this.showContent;
+  }
+
+  toggleOption() {
+    this.showOptions = !this.showOptions;
+  }
+
+  togglePendingOption() {
+    this.showPendingOptions = !this.showPendingOptions;
+  }
+
+  approvedFriendsRequest(user: UserSanitizeInterface) {
+    this.requestsService.updateFriendsStatus(user.id).subscribe(() => {
+      this.pendingFriends = this.pendingFriends.filter((element) => {
+        return element !== user
+      });
+      this.approvedFriends.push(user);
+    })
+  }
+
+  //delete a friend in the friends lists, then refresh the array of friends with the removed element
+  deleteFriends(user: UserSanitizeInterface) {
+    this.requestsService.deleteFriends(user.id).subscribe(() => {
+      this.approvedFriends = this.approvedFriends.filter((element) => {
+        return element !== user
+      });
+    });
+  }
+
+  showProfile() {
+
   }
   
 }
