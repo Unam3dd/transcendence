cdaf1647a9d74657130c71051d0ea548b0946d1e
Fenbjorn <ngrenoux@gmail.com>
2023-11-22 12:28:01 +0100

    begin navbar

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {UserInterface} from "../interfaces/user.interface";
+import {RequestsService} from "../services/requests.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  userData$!: Observable<UserInterface> | null;
+
+  constructor(private router: Router,
+              private requestsService: RequestsService) {}
+
+  ngOnInit() {
+    this.userData$ = this.requestsService.getLoggedUserInformation();
+  }
+
+  moveToHome() {
+    this.router.navigateByUrl('home');
+  }
 }
