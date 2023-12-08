ed38567f9ae32f426897307a1e7a5c257610ce66
mangestesmort <tagrandmerelapute@gmail.com>
2023-12-08 14:38:17 +0100

    putain

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,6 +1,6 @@
 const DEV_MODE = 0;
 const CLIENT_ID = 'u-s4t2ud-1797d9bf00415e2c434c540d187d18f12abb6be481dcc132c47cb84e823c1572';
-const IP_SERVER = 'localhost';
+const IP_SERVER = '10.13.249.195';
 const ANGULAR_DEV_PORT = 4200;
 const NEST_PORT = 3000;
 const WS_PORT = 3001;
